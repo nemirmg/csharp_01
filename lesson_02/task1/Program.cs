d96f0ca72bdb38197f48201bfe76b86e4741a9ea
Mihail Nemirovchenko <nemirovchenko@gmail.com>
2024-02-01 23:20:22 +0300

    add lesson_02:task2

--- lesson_02/task1/Program.cs
+++ lesson_02/task1/Program.cs
@@ -1,4 +1,4 @@
-﻿int [] arr = new int [5];
+﻿int[] arr = new int[5];
 arr[0] = 5;
 arr[1] = 7;
 arr[2] = 5;
@@ -11,4 +11,10 @@ Console.WriteLine(arr[2]);
 Console.WriteLine(arr[3]);
 Console.WriteLine(arr[4]);
 
-Console.WriteLine(arr[5]);
+// Console.WriteLine(arr[5]);
+
+int[] arr2 = new int[5] { 1, 2, 3, 4, 5 };
+
+int[] arr3 = { 6, 7, 8, 9, 0 };
+
+// python: arr = [6, 7, 8, 9, 0]
---
+++ lesson_02/task2/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+// while (i < n)
+while (i < arr.Length)
+{
+    arr[i] = i + 1;
+    // Console.Write(arr[i]);
+    // Console.Write(' ');
+    Console.Write($"{arr[i]} ");
+    // i = i + 1;
+    i++;
+}

4d978c911782fcf99399808229f93d006279eafb
Mihail Nemirovchenko <nemirovchenko@gmail.com>
2024-02-04 20:51:38 +0300

    add lesson_03:task5

---
+++ lesson_03/task5/Program.cs
@@ -0,0 +1,12 @@
+﻿// заполнение массива случайными значениями из интервала
+
+Random rnd = new Random();
+int size = 1000;
+int[] arr_int = new int[size];
+int i = 0;
+while (i < size)
+{
+    arr_int[i] = rnd.Next(1, 10);
+    Console.Write($"{arr_int[i]} ");
+    i = i + 1;
+}

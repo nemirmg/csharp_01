6d5315c49e92df9048c3804f29a2309d8877a53e
Mihail Nemirovchenko <nemirovchenko@gmail.com>
2024-02-11 20:07:32 +0300

    add lesson_04:task3

---
+++ lesson_04/task3/Program.cs
@@ -0,0 +1,14 @@
+﻿string GetLettersFromString(string line){
+    string letters = "";
+    foreach (char element in line){
+        if (char.IsLetter(element))
+            letters += element;
+    }
+    return letters;
+}
+
+Console.Clear();
+Console.Write("Введите строку символов: ");
+string str = Console.ReadLine()!;
+string result = GetLettersFromString(str);
+Console.WriteLine(result);

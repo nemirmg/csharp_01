c265c0a0622449d49df388a7af2e0cc52117c582
Mihail Nemirovchenko <nemirovchenko@gmail.com>
2024-02-11 17:25:55 +0300

    add lesson_04:task1

---
+++ lesson_04/task1/Program.cs
@@ -0,0 +1,25 @@
+﻿int[,] CreateMatrix(int rowCount, int columnCount)
+{
+    int[,] matrix = new int[rowCount, columnCount];
+
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+int[,] matrix = CreateMatrix(3, 5);
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]}\t");
+    }
+    Console.WriteLine();
+}
+

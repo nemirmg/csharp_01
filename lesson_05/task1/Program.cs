04f00f4b79192498e5bf8dc68d186ac8e3438dd7
Mihail Nemirovchenko <nemirovchenko@gmail.com>
2024-02-15 22:52:17 +0300

    add lesson_05:task1

---
+++ lesson_05/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿int fact(int n){
+    if (n == 1){
+        Console.WriteLine($"Stop: {n}");
+        return 1;
+    }
+    Console.WriteLine(n);
+    int result = fact(n - 1);
+    Console.WriteLine($"Возврат: n = {n}, fact = {result}");
+    return n * result; 
+}
+
+Console.WriteLine(fact(5));
